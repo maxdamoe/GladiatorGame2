fad1ba25e9c70bc8673df416898a8e7e75098c93
msimp <max.simpson@gmail.com>
2021-06-08 11:59:16 -0500

    3.1

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,7 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 var playerHealth = 100;
+var playerMoney = 10;
 
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
@@ -42,10 +43,19 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
     // if player choses to skip
   } else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(playerName + " has chosen to skip the fight!");
-  } else {
-    window.alert("You need to choose a valid option. Try again!");
-  }
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
+      fight();
+    }
 };
 
 
